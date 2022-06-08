23e2517a350ed3d0380b7e3e94e9c8a53244d1b1
Ross Chan <rlcchan@zallat.com>
2022-06-08 14:23:51 -0600

    - added unique month check during name generation to shenanigans where months are involved
    = Library =
    - added words
    - moved radio alphabet to nouns
    - removed superfluous November from radio alphabet

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const init = (function() {
-    const nameModal = new bootstrap.Modal('#namePreview');
+    const nameModal = new bootstrap.Modal('#namePreview');      // modal reference
+    const NAME_LIST_COUNT = 24;                                 // Number of names to generate
 
     // called by ship name buttons to open preview modal
     window.previewName = (shipName) => {
@@ -10,17 +11,20 @@ const init = (function() {
     }
 
     // get word from a specific word group in the library [adjectives, nouns]
-    getWord = (group) => {
-        let index = Math.floor(Math.random() * library[group].length);
+    getWord = (group, isMonth) => {
+        let index = isMonth
+            ? Math.floor(Math.random() * (library[group].length - 12)) + 12     // skip months
+            : Math.floor(Math.random() * library[group].length);
         return library[group][index];
     }
 
     // grab a unique word that is not a repeat of given parameter
-    getUniqueNoun = (shipName) => {
-        let uniqueNoun = getWord('nouns');
+    getUniqueNoun = (shipName, isMonth) => {
+        let uniqueNoun = getWord('nouns', isMonth);
         while(uniqueNoun === shipName) {
-            uniqueNoun = getWord('nouns');
+            uniqueNoun = getWord('nouns', isMonth);
         }
+        
         return uniqueNoun;
     }
 
@@ -28,36 +32,51 @@ const init = (function() {
     generateLongNameChance = (shipName) => {
         let randSecondary = Math.floor(Math.random() * 4);
         if(randSecondary) {
+            // check if shipName is a month
+            let months = library.nouns.slice(0,12);
+            let isMonth = months.find(el => el === shipName);
+
             // choose to add 'of' in between name
             if(Math.floor(Math.random() * 2)) {
-                let secondNoun = getUniqueNoun(shipName);
-                shipName = `${shipName} of ${secondNoun}`;
+                let secondNoun = getUniqueNoun(shipName, isMonth);
+                
+                // if primary ship name is month, set order accordingly
+                shipName = isMonth
+                    ? `${secondNoun} of ${shipName}`
+                    : `${shipName} of ${secondNoun}`;
             }
+            // prepend a noun or adjective
             else {
-                // use a noun or adjective
                 let randNoun = Math.floor(Math.random() * 2);
-
                 let word = randNoun
-                    ? getUniqueNoun(shipName)
+                    ? getUniqueNoun(shipName, isMonth)
                     : getWord('adjectives');
 
-                shipName = `${word} ${shipName}`;
+                // if primary ship name is month, set order accordingly
+                shipName = randNoun && isMonth
+                    ? `${shipName} ${word}`
+                    : `${word} ${shipName}`;
             }
         }
 
         return shipName;
     }
 
-    // generate a list of 16 ship names
+    // generate a list of ship names
     generateNames = () => {
         let shipNames = [];
-        for(let i=0; i<16; i++) {
+        let i = 0;
+        
+        while(i<NAME_LIST_COUNT) {
             let shipName = getWord('nouns');
             shipName = generateLongNameChance(shipName);
             
             // make sure there are no repeats
             const found = shipNames.find(el => el == shipName);
-            found ? i-- : shipNames.push(shipName);
+            if(!found) {
+                shipNames.push(shipName);
+                i++;
+            }
         }
         
         displayNames(shipNames);
